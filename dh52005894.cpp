5c35e8326fe09af2c5e11c6d9547bf6b7eaa250c
Khanh <khanh2282002dzz@gmail.com>
2023-11-13 13:32:14 +0700

    chinhphuong

--- dh52005894.cpp
+++ dh52005894.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
-
+bool isPerfectSquare(int number) {
+    int sqrtNumber = sqrt(number);
+    return (sqrtNumber * sqrtNumber == number);
+}
 void main(){
     int n;
     cout << "Nhap vao mot so nguyen ";
     cin >> n;
+    if (isPerfectSquare(n)) {
+        cout << n << " là số chính phương." << std::endl;
+    } else {
+        cout << n << " không phải là số chính phương." << std::endl;
+    }
 }
