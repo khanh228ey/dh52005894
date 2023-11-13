f4161954aeff4a69294842fc56b71fef7572b063
Khanh <khanh2282002dzz@gmail.com>
2023-11-13 13:34:22 +0700

    hoanthien

--- dh52005894.cpp
+++ dh52005894.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
+bool isPerfectNumber(int number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    int sum = 1;
+    for (int i = 2; i <= number / 2; i++) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+
+    return (sum == number);
+}
 
 void main(){
     int n;
     cout << "Nhap vao mot so nguyen ";
     cin >> n;
+    if (isPerfectNumber(n)) {
+        cout << n << " là số hoàn thiện." << endl;
+    } else {
+        cout << n << " không phải là số hoàn thiện." << :endl;
+    }
+
 }
